5ed575b3c61e1ea22c6ddb092da9e0522d9c7a2e
albertelg <98030807+albertelg@users.noreply.github.com>
2024-12-20 12:24:58 +0100

    Mov

--- FINAL PROJECT 3D/Assets/Resources/Scripts/PlayerControllerScript.cs
+++ FINAL PROJECT 3D/Assets/Resources/Scripts/PlayerControllerScript.cs
@@ -13,8 +13,17 @@ namespace UVic.jordipermanyerandalbertelgstrom.Vgame3D.fps
         public string team = "NoTeam"; // Team
         public string text = ""; // Text for the player info
 
+        private Rigidbody rb;
+        private Animator animator;
+        private Vector3 moveDir;
+        private bool isGrounded;
+        public float jumpForce = 25f;
+
         void Start()
         {
+            rb = GetComponent<Rigidbody>();
+            animator = GetComponent<Animator>();
+
             if (!photonView.IsMine)
             {
                 transform.GetChild(0).gameObject.SetActive(false); //disable camera
@@ -40,20 +49,10 @@ namespace UVic.jordipermanyerandalbertelgstrom.Vgame3D.fps
         {
             if (photonView.IsMine) //only moves if the player is yourself
             {
-                float horizontal = Input.GetAxis("Horizontal");
-                float vertical = Input.GetAxis("Vertical");
-
-                Vector3 movement = new Vector3(horizontal, 0, vertical);
-                transform.Translate(movement * speed * Time.deltaTime, Space.World);
-
-                // Rotación con el mouse (eje X)
-                float mouseX = Input.GetAxis("Mouse X");
-                if (Mathf.Abs(mouseX) > 0.01f)
-                {
-                    transform.Rotate(Vector3.up, mouseX * speed * Time.deltaTime);
-                }
+                movment();
             }
         }
+
         public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
         {
             if (stream.IsWriting)
@@ -69,6 +68,79 @@ namespace UVic.jordipermanyerandalbertelgstrom.Vgame3D.fps
         {
             GUI.TextArea(new Rect(20, 120, 300, 100), text);
         }
+
+
+        /* LOGICA MOVIMENT */
+
+        public void movment()
+        {
+            /*
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(horizontal, 0, vertical);
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+            // Rotación con el mouse (eje X)
+            float mouseX = Input.GetAxis("Mouse X");
+            if (Mathf.Abs(mouseX) > 0.01f)
+            {
+                transform.Rotate(Vector3.up, mouseX * speed * Time.deltaTime);
+            }
+            */
+            GetInput();
+            Jump();
+            UpdateAnimationState();
+        }
+
+        private void GetInput()
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            Vector3 targetVelocity = moveDir * speed;
+            if (!isGrounded && rb.velocity.y > 0)
+            {
+                targetVelocity *= 0.9f; // Reduce speed a bit when moving upwards
+            }
+
+            rb.velocity = new Vector3(targetVelocity.x, rb.velocity.y, targetVelocity.z);
+        }
+
+        private void Jump()
+        {
+            if (isGrounded && Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); // Reset vertical velocity before jumping
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Apply jump force
+            }
+        }
+
+        private void UpdateAnimationState()
+        {
+            if (moveDir == Vector3.zero && isGrounded) // Idle state
+            {
+                animator.SetInteger("state", 0);
+            }
+            else if (isGrounded) // Walking state
+            {
+                animator.SetInteger("state", 1);
+            }
+            else if (!isGrounded) // Jumping state
+            {
+                animator.SetInteger("state", 3);
+            }
+        }
+
+
+
+
+
+
+
     }
+
+
+    
 }
 
