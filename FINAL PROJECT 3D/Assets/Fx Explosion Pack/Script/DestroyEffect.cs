b3d31166e3bb7010d04bd5815e661d5aa3b92aa2
Jordi Permanyer <74786713+jordipermanyer@users.noreply.github.com>
2025-01-13 17:54:25 +0100

    a
    a

---
+++ FINAL PROJECT 3D/Assets/Fx Explosion Pack/Script/DestroyEffect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEffect : MonoBehaviour 
+{
+    void Start()
+    {
+        // Se destruirá automáticamente a los 2 segundos
+        Destroy(gameObject, 2f);
+    }
+}
